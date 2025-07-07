f2ad8065ebf5823cfd844409dc77aa025b60a5d4
fz <ardestanifazele@gmail.com>
2025-07-07 19:58:52 +0330

    feat(except): Define exceptions for user authentication.

---
+++ exeptions.py
@@ -0,0 +1,17 @@
+class UsernameExistsError(Exception):
+    """Raised when trying to register with a username that is already taken."""
+    pass
+
+class InvalidPasswordError(Exception):
+    """Raised when a password does not meet the security criteria (e.g., length)."""
+    pass
+
+class PasswordsDoesNotMatchError(Exception):
+    """Raised when password and confirmation password do not match."""
+    pass
+
+class InvalidCredentialsError(Exception):
+    """Raised for login attempts with wrong username or password."""
+    pass
+
+
