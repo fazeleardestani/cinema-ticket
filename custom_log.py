0d8f73f24003ada51657245d4eceab39c05134fb
fz <ardestanifazele@gmail.com>
2025-07-12 20:15:59 +0330

    feat(custom-log): Add stream handler for terminal output

--- custom_log.py
+++ custom_log.py
@@ -1,7 +1,8 @@
 """Configures the application logger to write to cinematicket.log."""
 import logging
 
-FORMAT = '%(asctime)s - %(levelname)s - %(name)s - %(message)s'
+FORMAT = ('%(asctime)s - %(levelname)s \n\t'
+          '--> %(message)s')
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -10,7 +11,12 @@ file_handler = logging.FileHandler('cinematicket.log')
 file_formatter = logging.Formatter(FORMAT)
 file_handler.setFormatter(file_formatter)
 
+stream_handler = logging.StreamHandler()
+stream_formatter = logging.Formatter(FORMAT)
+stream_handler.setFormatter(stream_formatter)
+
 logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
 
 
 
