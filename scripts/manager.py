fa8210c56abc8eaf2a7e6b61dd1fb7a8fb154a09
fz <ardestanifazele@gmail.com>
2025-09-05 22:03:38 +0330

    feat(admin): Create CLI for cinema management
    - Added a new `scripts/manager.py` to serve as the entry point for administrators.
    - Implemented `argparse` to handle command-line arguments for creating new movie showings with time, capacity, and price.
    - Integrated a secure admin-only login function that verifies the user's role before granting access to commands.
    - Added system path modification to ensure correct module imports when run from any location.

--- scripts/manager.py
+++ scripts/manager.py
@@ -1,6 +1,11 @@
 import argparse
 import getpass
 import os
+import sys
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, project_root)
+
 from custom_log import logger as log
 from exeptions import InvalidAccess
 from models.cinema import Showing, Movies
@@ -26,6 +31,7 @@ def admin_login()-> User | None:
 parser = argparse.ArgumentParser(description='Cinema Ticket Management')
 
 parser.add_argument('--movie-title' , type=str, help='Movie Title' , required=True)
+parser.add_argument('--time' , type=str, help='Showing time in "YYYY-M-D HH:MM" format' , required=True)
 parser.add_argument('--capacity' , type=int, help='Total number of seats' , required=True)
 parser.add_argument('--age-group' , type=int, help='Minimum age for the movie' , required=True)
 parser.add_argument('--price' , type=int, help='Ticket price' , required=True)
@@ -36,12 +42,13 @@ try:
     admin_login()
 
     print(f"Creating showing for movie: {args.movie_title}")
+    print(f"Time: {args.time}")
     print(f"Capacity: {args.capacity}")
     print(f"Age Group: {args.age_group}")
     print(f"Price: {args.price}")
 
     movie = Movies(args.movie_title , args.age_group)
-    Showing.create_showing(movie, args.capacity, args.price)
+    Showing.create_showing(movie, args.capacity, args.price , args.time)
     print("\nShowing created successfully!")
 except Exception as e:
     print(e)
