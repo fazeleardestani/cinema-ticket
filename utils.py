97c7ec18eebe136e3d078c876adfb85a6ed79f98
fz <ardestanifazele@gmail.com>
2025-09-05 21:59:01 +0330

    refactor(utils): Specialize date parsing and add calculators
    - Split the generic str_to_datetime into `validate_birth_date` and `str_to_showing_datetime` to handle different formats robustly.
    - Added `calculate_time_span` for jdatetime timedelta calculations.
    - Added `apply_discount` for calculating prices after discounts.

--- utils.py
+++ utils.py
@@ -1,5 +1,15 @@
 import hashlib
 import json
+import re
+
+from datetime import datetime
+
+import jdatetime
+from django.utils.formats import date_format
+
+from custom_log import logger as log
+from exeptions import InvalidDateError
+
 
 def data_dump(file_path:str , data:list):
     """Saves a dictionary to a JSON file."""
@@ -21,3 +31,38 @@ def hash_password(password: str) -> str:
     """Hashes the password using SHA-256."""
     return hashlib.sha256(password.encode('utf8')).hexdigest()
 
+def str_to_datetime(date:str):
+    """
+        Converts a date string in 'YYYY-M-D' or 'YYYY-MM-DD' format to a jdatetime object.
+        Raises InvalidDateError if the format is incorrect.
+        """
+    date_format = "%Y-%m-%d"
+    try:
+        new_date = jdatetime.datetime.strptime(date, date_format)
+        return new_date
+
+    except (ValueError , TypeError):
+        log.warning("Validation failed for date: '{}'. Format should be YYYY-M-D or YYYY-MM-DD.".format(date))
+        raise InvalidDateError
+
+def str_to_showimg_datetime(datetime_str:str):
+    """Converts a showing datetime string ('YYYY-M-D HH:MM') to a jdatetime object."""
+    datetime_format = "%Y-%m-%d %H:%M"
+    try:
+        new_datetime = jdatetime.datetime.strptime(datetime_str, datetime_format)
+        return new_datetime
+    except (ValueError , TypeError):
+        log.warning(f"Validation failed for showing time: '{datetime_str}'. Format should be YYYY-M-D HH:MM.")
+        raise InvalidDateError
+
+def calculate_time_span(start:jdatetime.datetime , end:jdatetime.datetime):
+    """
+    Calculates the time difference (span) between two time.
+    """
+    time_span = end - start
+    return time_span
+
+
+def apply_discount(price:int , discount:int):
+    discount_amount = int((price * discount)/100)
+    return price - discount_amount
